48e3b73d6128ec81f50ce9fc73b6e868f2b1cc26
Pubudu Janith <pubudujanith123@gmail.com>
2023-10-21 21:43:51 +0530

    Add DB transactions

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,14 +2,22 @@ const {db} = require("../models/index");
 const {ConflictError, NotFoundError} = require("../errors/error");
 
 const Student = db.students;
+const sequelize = db.sequelize;
 
 const createNewStudent = async (req, res) => {
-    const availability = await Student.findByPk(req.body.nic);
-    if (!availability) {
-        const createdStudent = await Student.create(req.body);
-        return res.status(201).json(createdStudent);
+    const t = await sequelize.transaction();
+    try {
+        const availability = await Student.findByPk(req.body.nic);
+        if (!availability) {
+            const createdStudent = await Student.create(req.body);
+            await t.commit();
+            return res.status(201).json(createdStudent);
+        }
+        throw new ConflictError("User already exit in the database", 409);
+    } catch (error) {
+        await t.rollback();
+        throw error;
     }
-    throw new ConflictError("User already exit in the database", 409);
 }
 
 const getStudentDetails = async (req, res) => {
@@ -20,23 +28,37 @@ const getStudentDetails = async (req, res) => {
 }
 
 const updateStudent = async (req, res) => {
-    const id = req.params.nic;
-    const student = await Student.findByPk(id);
-    if (student) {
-        await Student.update(req.body, {where: {nic: id}});
-        return res.status(201).json(req.body);
+    const t = await sequelize.transaction();
+    try {
+        const id = req.params.nic;
+        const student = await Student.findByPk(id);
+        if (student) {
+            await Student.update(req.body, {where: {nic: id}});
+            await t.commit();
+            return res.status(201).json(req.body);
+        }
+        throw new NotFoundError("Student doesn't exist", 404);
+    } catch (error) {
+        await t.rollback();
+        throw error;
     }
-    throw new NotFoundError("Student doesn't exist", 404);
 }
 
 const deleteStudent = async (req, res) => {
-    const id = req.params.nic;
-    const student = await Student.findByPk(id);
-    if (student) {
-        await Student.destroy({where: {nic: id}});
-        return res.status(204).json({});
+    const t = await sequelize.transaction();
+    try {
+        const id = req.params.nic;
+        const student = await Student.findByPk(id);
+        if (student) {
+            await Student.destroy({where: {nic: id}});
+            await t.commit();
+            return res.status(204).json({});
+        }
+        throw new NotFoundError("Student doesn't exist", 404);
+    } catch (error) {
+        await t.rollback();
+        throw error;
     }
-    throw new NotFoundError("Student doesn't exist", 404);
 }
 
 module.exports = {createNewStudent,getStudentDetails,updateStudent,deleteStudent}
